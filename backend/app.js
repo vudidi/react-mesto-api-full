2bb635fd5a8f78757362766ecedc777fc83fc6cb
Vika Dovgal <vikadovgal@yandex.ru>
2022-06-13 02:45:10 +0400

    cors

--- backend/app.js
+++ backend/app.js
@@ -23,12 +23,8 @@ const allowedCors = [
 app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin X-Requested-With, Content-Type, Accept, Authorization'
-  );
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -37,9 +33,12 @@ app.use((req, res, next) => {
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
   }
 
-  next();
+  return next();
 });
 
 // app.use(cors());
