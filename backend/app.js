79c7c191868acab0bcd17ec6ce75f3d96b752e05
Vika Dovgal <vikadovgal@yandex.ru>
2022-06-14 22:57:20 +0400

    ревью

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', userRouter);
 app.use('/', cardRouter);
 app.use('*', auth, pageNotFound);
